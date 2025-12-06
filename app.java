70544af421ceaa56d61ee48d0abbdffe360c1a55
honey868 <94173431+honey868@users.noreply.github.com>
2025-12-06 09:24:20 +0530

    added new code
    added new code

--- app.java
+++ app.java
@@ -16,3 +16,19 @@ public class AddNumbers {
         input.close();
     }
 }
+
+public class Factorial {
+    public static int calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 5;
+        int result = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+    }
+}
